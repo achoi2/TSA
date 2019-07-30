f5a6f8a5bfd61cccc310def8c7ab1ab67e02699e
Andrew <andrewchoi1@hotmail.com>
2019-07-30 18:31:24 -0400

    added tone analyser

--- server/Route/tweet.js
+++ server/Route/tweet.js
@@ -1,7 +1,29 @@
-const analyze_tweet = (req, res) => {
-    const tweet = req.body.tweet
-    
-    console.log(tweet)
+const keys = require('../keys')
+const fetch = require('node-fetch');
+const ToneAnalyzerV3 = require('watson-developer-cloud/tone-analyzer/v3');
+
+const toneAnalyzer = new ToneAnalyzerV3({
+    version: '2017-09-21',
+    iam_apikey: `${keys.apiKey}`,
+});
+
+const analyze_tweet = async (req, res) => {
+    const tweet = (req.body.tweet)
+
+    const toneParams = {
+        tone_input: {'text': tweet},
+        content_type: 'application/json'
+    }
+
+    console.log(toneParams.tone_input)
+
+    toneAnalyzer.tone(toneParams)
+        .then(toneAnalysis => {
+            console.log(JSON.stringify(toneAnalysis, null, 2))
+        })
+        .catch(err => {
+            console.log('error:', err);
+        });
 } 
 
 module.exports = {
