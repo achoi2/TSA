795bcde5f4db7ed6bca545ba5787a8da38cddd0e
Andrew <andrewchoi1@hotmail.com>
2019-07-31 16:17:56 -0400

    added styles to form and viewer

--- client/src/components/AnalysisForm/AnalysisForm.js
+++ client/src/components/AnalysisForm/AnalysisForm.js
@@ -1,18 +1,20 @@
 import React from 'react'
 import { analyzeTweet } from '../../core/actions/tweet'
 import { connect } from 'react-redux'
+import './AnalysisForm.css'
 
 const AnalysisForm = (props) => {
     const onFormSubmit = (e) => {
         e.preventDefault()
         props.analyzeTweet(e.target.tweetText.value)
+        
     }
     
     return(
         <div>
-            <form onSubmit={onFormSubmit}>
-                <input type="text" placeholder="Analyze my tweet" name="tweetText"/>
-                <button>Analyze</button>
+            <form onSubmit={onFormSubmit} className="analyze-form">
+                <textarea type="text" placeholder="Analyze my tweet" name="tweetText"/>
+                <button className="analyze-button">Analyze</button>
             </form>
         </div>
     )
