6d8c07e4b52a59fe060e41938f47833e49f5b7e7
Andrew <andrewchoi1@hotmail.com>
2019-08-12 14:46:42 -0400

    added backend twitter api

--- server/Route/twitter.js
+++ server/Route/twitter.js
@@ -1,8 +1,25 @@
 const Twit = require('twit')
+const  { twitterAPI, twitterSecret, accessToken, accessSecret } = require('../keys')
 
 var T = new Twit({
-    consumer_key: '...',
-    consumer_secret: '...',
-    access_token: '...',
-    access_token_secret: '...'
+    consumer_key: twitterAPI,
+    consumer_secret: twitterSecret,
+    access_token: accessToken,
+    access_token_secret: accessSecret
 })
+
+const post_tweet = async (req, res) => {
+    const tweet = req.body.tweet
+
+    try {
+        const send = await T.post('statuses/update', {status: tweet})
+        const data = send.json()
+        console.log(data)
+    } catch(e) {
+        res.send(e)
+    }
+}
+
+module.exports = {
+    post_tweet
+}
--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 const tweet = require('./Route/tweet')
+const twitter = require('./Route/twitter')
 const bodyParser = require('body-parser')
 
 app.use(bodyParser.json())
@@ -11,5 +12,8 @@ app.use(bodyParser.urlencoded({
 // tweet
 app.post('/tweet/save', tweet.save_tweet)
 app.post('/tweet/analyze', tweet.analyze_tweet)
+
+// twitter
+app.post('/twitter/post', twitter.post_tweet)
 
 app.listen(5000, () => console.log('listening on 5000'))
